fb7cfef8ffc5c5bbe96195fb726f35142c7b7699
kelly <kelly@ctdatahaven.org>
2020-07-16 17:33:53 -0400

    pull in more variables from hmda raw data

--- _utils/filter_hmda.R
+++ _utils/filter_hmda.R
@@ -4,8 +4,8 @@ library(vroom)
 # HMDA loan application data (LAR)
 # data from: https://ffiec.cfpb.gov/data-publication/dynamic-national-loan-level-dataset/2019
 # codebook: https://ffiec.cfpb.gov/documentation/2019/lar-data-fields/
-# 7GB raw data split in terminal, stitched together in database, and filtered here.
-# raw data are zipped up on black external hd if needed again
+# 7GB raw data split into 9 files of 2M rows in terminal with top row appended
+# raw data are zipped up on black external hd
 
 in_path <- "~/Desktop/hmda/"
 
@@ -14,7 +14,8 @@ file_list <- list.files(path = in_path)
 out <- file_list %>% map(~vroom(file = paste(in_path, ., sep = ""), delim = "|") %>%
 										filter(state_code == "CT", loan_purpose == 1, action_taken == 1,
 													 grepl("Single", derived_dwelling_category)) %>%
-										select(state_code, county_code, interest_rate, loan_amount, total_units,
+										select(state_code, county_code, interest_rate, loan_amount,
+													 total_units, hoepa_status, income,
 													 starts_with("derived"), starts_with("tract")) %>% 
 											mutate(total_units = as.numeric(total_units)))
 
